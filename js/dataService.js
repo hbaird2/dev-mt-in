99f35166b38f7b281f38c32f7a3fe7db31c87e11
rachel baird <rachelbaird@Macintosh-4.local>
2016-11-28 21:31:52 -0700

    commented out service

--- js/dataService.js
+++ js/dataService.js
@@ -1,33 +1,33 @@
 var ourService = angular.module('DevMtn').service('dataService', function($http){
 
-  this.getData = function() {
-    return $http({
-      method: 'GET',
-      url: 'http://connections.devmountain.com/api/profiles?name=t'
-    }).then(function(response){
-      return response;
-    }, function(error){
-      alert("error");
-    })
-  }
-
-
-
-
-  this.postData = function(user) {
-    var ourService = this;
-    console.log(user)
-    return $http({
-      method: 'POST',
-      url: 'http://connections.devmountain.com/api/profiles',
-      data: user
-    }).then(function(response){
-      // console.log("this is test", ourService)
-      ourService.newVariable = response;
-      return response;
-    }, function(error){
-      alert("error");
-    })
-  }
+  // this.getData = function() {
+  //   return $http({
+  //     method: 'GET',
+  //     url: 'http://connections.devmountain.com/api/profiles?name=t'
+  //   }).then(function(response){
+  //     return response;
+  //   }, function(error){
+  //     alert("error");
+  //   })
+  // }
+  //
+  //
+  //
+  //
+  // this.postData = function(user) {
+  //   var ourService = this;
+  //   console.log(user)
+  //   return $http({
+  //     method: 'POST',
+  //     url: 'http://connections.devmountain.com/api/profiles',
+  //     data: user
+  //   }).then(function(response){
+  //     // console.log("this is test", ourService)
+  //     ourService.newVariable = response;
+  //     return response;
+  //   }, function(error){
+  //     alert("error");
+  //   })
+  // }
 
 });
